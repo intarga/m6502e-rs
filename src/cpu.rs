cc27b85e992bfae54d9997d0e71aa05ebc69de41
Ingrid <git@ingrids.email>
2022-04-14 14:07:32 +0200

    implement AddressingModes as an enum

--- src/cpu.rs
+++ src/cpu.rs
@@ -9,6 +9,17 @@ pub struct CpuState {
     p: u8,
 }
 
+enum AddressingMode {
+    I,     // Immediate
+    A,     // Absolute
+    DP,    // Direct Page
+    AIX,   // Absolute Indexed X
+    AIY,   // Absolute Indexed Y
+    DPIX,  // Direct Page Indexed X
+    DPIIX, // Direct Page Indexed Indirect X
+    DPIIY, // Direct Page Indexed Indirect Y
+}
+
 pub fn emulate_op(opcode: u8, state: &mut CpuState) {
     match opcode {
         0x00 => state.a = 0xff,
