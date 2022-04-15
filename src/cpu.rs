a4b508d7b328d4dba97a835f4017d6fae38b2471
Ingrid <git@ingrids.email>
2022-04-15 18:41:59 +0200

    implement ADC remaining addressing modes

--- src/cpu.rs
+++ src/cpu.rs
@@ -6,14 +6,14 @@ pub struct CpuState {
     y: u8,
     pch: u8,
     pcl: u8,
-    s: u8,
+    // s: u8,
 
     // flags
     negative: bool,
     signed_overflow: bool,
-    brk_interrupt: bool,
+    // brk_interrupt: bool,
     decimal_mode: bool,
-    irq_interrupt_disable: bool,
+    // irq_interrupt_disable: bool,
     zero: bool,
     carry: bool,
 }
@@ -36,12 +36,12 @@ impl Default for SystemState {
 enum AddressingMode {
     I,     // Immediate
     A,     // Absolute
-    DP,    // Direct Page
-    AIX,   // Absolute Indexed X
-    AIY,   // Absolute Indexed Y
-    DPIX,  // Direct Page Indexed X
-    DPIIX, // Direct Page Indexed Indirect X
-    DPIIY, // Direct Page Indexed Indirect Y
+    Zp,    // Zero Page
+    Aix,   // Absolute Indexed X
+    Aiy,   // Absolute Indexed Y
+    Zpix,  // Zero Page Indexed X
+    Zpiix, // Zero Page Indexed Indirect X
+    Zpiiy, // Zero Page Indirect Indexed Y
 }
 
 // -- Helper functions --
@@ -57,6 +57,73 @@ fn cat_bytes(b1: u8, b2: u8) -> u16 {
 fn get_immediate_byte(sys: &mut SystemState, offset: u16) -> u8 {
     let addr = cat_bytes(sys.cpu_state.pch, sys.cpu_state.pcl) + offset;
     get_byte_at_addr(sys, addr)
+}
+
+fn get_absolute_byte(sys: &mut SystemState) -> u8 {
+    let addr_lo = get_immediate_byte(sys, 1);
+    let addr_hi = get_immediate_byte(sys, 2);
+    let addr = cat_bytes(addr_hi, addr_lo);
+
+    get_byte_at_addr(sys, addr)
+}
+
+fn get_absolute_byte_indexed(sys: &mut SystemState, index: u8) -> (u8, bool) {
+    let mut addr_lo = get_immediate_byte(sys, 1);
+    let mut addr_hi = get_immediate_byte(sys, 2);
+
+    let carry: bool;
+    (addr_lo, carry) = addr_lo.overflowing_add(index);
+
+    if carry {
+        addr_hi = addr_hi
+            .checked_add(1)
+            .expect("Overflow indexing memory address")
+    }
+
+    let addr = cat_bytes(addr_hi, addr_lo);
+
+    (get_byte_at_addr(sys, addr), carry)
+}
+
+fn get_zero_page_byte(sys: &mut SystemState) -> u8 {
+    let addr = get_immediate_byte(sys, 1) as u16;
+    get_byte_at_addr(sys, addr)
+}
+
+fn get_zero_page_byte_indexed(sys: &mut SystemState, index: u8) -> u8 {
+    let addr = get_immediate_byte(sys, 1).wrapping_add(index) as u16;
+    get_byte_at_addr(sys, addr)
+}
+
+fn get_zero_page_byte_indexed_indirect(sys: &mut SystemState, index: u8) -> u8 {
+    let addr1 = get_immediate_byte(sys, 1).wrapping_add(index) as u16;
+
+    let addr2_lo = get_byte_at_addr(sys, addr1);
+    let addr2_hi = get_byte_at_addr(sys, (addr1 + 1) & 0xff);
+    let addr2 = cat_bytes(addr2_hi, addr2_lo);
+
+    get_byte_at_addr(sys, addr2)
+}
+
+fn get_zero_page_byte_indirect_indexed(sys: &mut SystemState, index: u8) -> (u8, bool) {
+    //
+    let addr1 = get_immediate_byte(sys, 1) as u16;
+
+    let mut addr2_lo = get_byte_at_addr(sys, addr1);
+    let mut addr2_hi = get_byte_at_addr(sys, (addr1 + 1) & 0xff);
+
+    let carry: bool;
+    (addr2_lo, carry) = addr2_lo.overflowing_add(index);
+
+    if carry {
+        addr2_hi = addr2_hi
+            .checked_add(1)
+            .expect("Overflow indexing memory address")
+    }
+
+    let addr2 = cat_bytes(addr2_hi, addr2_lo);
+
+    (get_byte_at_addr(sys, addr2), carry)
 }
 
 fn increment_pc(sys: &mut SystemState, num: u8) {
@@ -86,12 +153,34 @@ fn set_n_z(sys: &mut SystemState, result: u8) {
 fn adc(sys: &mut SystemState, mode: AddressingMode) -> (u8, u8) {
     let (operand, length, cycles) = match mode {
         AddressingMode::I => (get_immediate_byte(sys, 1), 2, 2),
-        _ => panic!("unsupported mode {:?} on instruction ADC", mode),
+        AddressingMode::A => (get_absolute_byte(sys), 3, 4),
+        AddressingMode::Zp => (get_zero_page_byte(sys), 2, 3),
+        AddressingMode::Aix => {
+            let (byte, page_cross) = get_absolute_byte_indexed(sys, sys.cpu_state.x);
+            (byte, 3, 4 + page_cross as u8)
+        }
+        AddressingMode::Aiy => {
+            let (byte, page_cross) = get_absolute_byte_indexed(sys, sys.cpu_state.y);
+            (byte, 3, 4 + page_cross as u8)
+        }
+        AddressingMode::Zpix => (get_zero_page_byte_indexed(sys, sys.cpu_state.x), 2, 4),
+        AddressingMode::Zpiix => (
+            get_zero_page_byte_indexed_indirect(sys, sys.cpu_state.x),
+            2,
+            6,
+        ),
+        AddressingMode::Zpiiy => {
+            let (byte, page_cross) = get_zero_page_byte_indirect_indexed(sys, sys.cpu_state.y);
+            (byte, 2, 5 + page_cross as u8)
+        } // _ => panic!("unsupported mode {:?} on instruction ADC", mode),
     };
+    if sys.cpu_state.decimal_mode {
+        panic!("Decimal addition not implemented yet!")
+    }
 
     let negative_before = negative_u8(sys.cpu_state.a);
-
     let (carry1, carry2): (bool, bool);
+
     (sys.cpu_state.a, carry1) = sys.cpu_state.a.overflowing_add(operand);
     (sys.cpu_state.a, carry2) = sys.cpu_state.a.overflowing_add(sys.cpu_state.carry as u8);
 
@@ -106,7 +195,14 @@ pub fn emulate_op(sys: &mut SystemState) -> u8 {
     let opcode = get_immediate_byte(sys, 0);
 
     let (length, cyc) = match opcode {
+        0x61 => adc(sys, AddressingMode::Zpiix),
+        0x65 => adc(sys, AddressingMode::Zp),
         0x69 => adc(sys, AddressingMode::I),
+        0x6d => adc(sys, AddressingMode::A),
+        0x71 => adc(sys, AddressingMode::Zpiiy),
+        0x75 => adc(sys, AddressingMode::Zpix),
+        0x79 => adc(sys, AddressingMode::Aiy),
+        0x7d => adc(sys, AddressingMode::Aix),
         _ => panic!("unimplemented instruction {}", opcode),
     };
 
